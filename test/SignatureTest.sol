21e15a4015861ab704d55c7cb6cd995ca1f878ba
Quentin Garchery <garchery.quentin@gmail.com>
2025-11-12 15:19:30 +0100

    fix: simplify signature test

--- test/SignatureTest.sol
+++ test/SignatureTest.sol
@@ -8,21 +8,11 @@ import {ROOT_TYPEHASH} from "../src/libraries/ConstantsLib.sol";
 import {Test} from "../lib/forge-std/src/Test.sol";
 
 contract SignatureTest is Test, MorphoV2 {
-    function testFormat(bytes32 root) public view {
+    function testSigner(bytes32 root, uint256 privateKey) public view {
+        privateKey = boundPrivateKey(privateKey);
         bytes32 structHash = keccak256(abi.encode(ROOT_TYPEHASH, root, block.chainid, address(this)));
-        bytes32 expectedDigest = keccak256(bytes.concat("\x19\x01", _domainSeparator(), structHash));
-        assertEq(expectedDigest, _hashTypedData(root));
-    }
-
-    function testSigner(bytes32 root, uint256 sk) public view {
-        sk = boundPrivateKey(sk);
-        bytes32 digest = _hashTypedData(root);
-        (uint8 v, bytes32 r, bytes32 s) = vm.sign(sk, digest);
-        assertEq(_signer(root, Signature({v: v, r: r, s: s})), vm.addr(sk));
-    }
-
-    function _hashTypedData(bytes32 root) internal view returns (bytes32) {
-        bytes32 structHash = keccak256(abi.encode(ROOT_TYPEHASH, root, block.chainid, address(this)));
-        return keccak256(bytes.concat("\x19\x01", _domainSeparator(), structHash));
+        bytes32 digest = keccak256(bytes.concat("\x19\x01", _domainSeparator(), structHash));
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(privateKey, digest);
+        assertEq(_signer(root, Signature({v: v, r: r, s: s})), vm.addr(privateKey));
     }
 }
