6e228458e86211ef5733e34015d1d76ebb6aae0f
MathisGD <mathis@morpho.xyz>
2026-01-23 17:50:25 +0100

    perf

--- src/MorphoV2.sol
+++ src/MorphoV2.sol
@@ -163,8 +163,7 @@ contract MorphoV2 is IMorphoV2 {
         require(signer(root, sig) == offer.maker, "invalid signature");
         require(UtilsLib.isLeaf(root, keccak256(abi.encode(offer)), proof), "invalid proof");
         require(offer.session == session[offer.maker], "invalid session");
-        bytes32 id = toId(offer.obligation);
-        touchObligation(offer.obligation);
+        bytes32 id = touchObligation(offer.obligation);
 
         (
             address buyer,
@@ -290,8 +289,7 @@ contract MorphoV2 is IMorphoV2 {
         returns (uint256, uint256)
     {
         require(UtilsLib.atMostOneNonZero(obligationUnits, shares), "INCONSISTENT_INPUT");
-        bytes32 id = toId(obligation);
-        touchObligation(obligation);
+        bytes32 id = touchObligation(obligation);
 
         if (obligationUnits > 0) shares = obligationUnits.mulDivUp(totalShares[id] + 1, totalUnits[id] + 1);
         else obligationUnits = shares.mulDivDown(totalUnits[id] + 1, totalShares[id] + 1);
@@ -310,8 +308,7 @@ contract MorphoV2 is IMorphoV2 {
     }
 
     function repay(Obligation memory obligation, uint256 obligationUnits, address onBehalf) external {
-        bytes32 id = toId(obligation);
-        touchObligation(obligation);
+        bytes32 id = touchObligation(obligation);
 
         debtOf[onBehalf][id] -= obligationUnits;
         withdrawable[id] += obligationUnits;
@@ -324,8 +321,7 @@ contract MorphoV2 is IMorphoV2 {
     function supplyCollateral(Obligation memory obligation, address collateral, uint256 assets, address onBehalf)
         external
     {
-        bytes32 id = toId(obligation);
-        touchObligation(obligation);
+        bytes32 id = touchObligation(obligation);
 
         collateralOf[onBehalf][id][collateral] += assets;
 
@@ -337,8 +333,7 @@ contract MorphoV2 is IMorphoV2 {
     function withdrawCollateral(Obligation memory obligation, address collateral, uint256 assets, address onBehalf)
         external
     {
-        bytes32 id = toId(obligation);
-        touchObligation(obligation);
+        bytes32 id = touchObligation(obligation);
 
         collateralOf[onBehalf][id][collateral] -= assets;
 
@@ -365,7 +360,7 @@ contract MorphoV2 is IMorphoV2 {
     {
         uint256 repayableDebt;
         uint256 maxDebt;
-        bytes32 id = toId(obligation);
+        bytes32 id = touchObligation(obligation);
         uint256[] memory prices = new uint256[](obligation.collaterals.length);
 
         for (uint256 i = 0; i < obligation.collaterals.length; i++) {
@@ -452,12 +447,13 @@ contract MorphoV2 is IMorphoV2 {
         SafeTransferLib.safeTransferFrom(token, msg.sender, address(this), assets);
     }
 
-    function touchObligation(Obligation memory obligation) internal {
+    function touchObligation(Obligation memory obligation) internal returns (bytes32) {
         bytes32 id = toId(obligation);
         if (!obligationCreated[id]) {
             emit EventsLib.ObligationCreated(id, obligation);
             obligationCreated[id] = true;
         }
+        return id;
     }
 
     /// VIEW FUNCTIONS ///
