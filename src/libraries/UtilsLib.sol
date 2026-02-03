6a88dcdb7464a3beab795c949debf0ab630a8536
MathisGD <mathis@morpho.xyz>
2026-02-03 14:44:49 +0100

    chore: revert utilslib change

--- src/libraries/UtilsLib.sol
+++ src/libraries/UtilsLib.sol
@@ -5,34 +5,34 @@ pragma solidity ^0.8.0;
 library UtilsLib {
     /// @dev Returns true if at most one of `x` and `y` is nonzero.
     function atMostOneNonZero(uint256 x, uint256 y) internal pure returns (bool z) {
-        assembly ("memory-safe") {
+        assembly {
             z := gt(add(iszero(x), iszero(y)), 0)
         }
     }
 
     /// @dev Returns true if at most one of `x`, `y`, `z` is nonzero.
     function atMostOneNonZero(uint256 a, uint256 b, uint256 c) internal pure returns (bool z) {
-        assembly ("memory-safe") {
+        assembly {
             z := gt(add(add(iszero(a), iszero(b)), iszero(c)), 1)
         }
     }
 
     /// @dev Returns true if at most one of `a`, `b`, `c`, `d` is nonzero.
     function atMostOneNonZero(uint256 a, uint256 b, uint256 c, uint256 d) internal pure returns (bool z) {
-        assembly ("memory-safe") {
+        assembly {
             z := gt(add(add(add(iszero(a), iszero(b)), iszero(c)), iszero(d)), 2)
         }
     }
 
     /// @dev Returns min(a, b).
     function min(uint256 x, uint256 y) internal pure returns (uint256 z) {
-        assembly ("memory-safe") {
+        assembly {
             z := xor(x, mul(xor(x, y), lt(y, x)))
         }
     }
 
     function zeroFloorSub(uint256 x, uint256 y) internal pure returns (uint256 z) {
-        assembly ("memory-safe") {
+        assembly {
             z := mul(gt(x, y), sub(x, y))
         }
     }
